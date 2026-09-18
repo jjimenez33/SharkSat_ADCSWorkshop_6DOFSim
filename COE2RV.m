768c60af61b7cb25937ed611d76d41c30b11f48f
Justin Jimenez <justinjimenez802@gmail.com>
2026-09-18 00:33:28 -0700

    Let's see if this works

---
+++ COE2RV.m
@@ -0,0 +1,78 @@
+function [r0, v0] = COE2RV(muEarth, COE)
+
+%% ---Extract COEs from array--- %%
+mean_motion       = COE(1);
+semi_major_axis   = COE(2);
+eccentricity      = COE(3);
+inclination       = COE(4);
+RAAN              = COE(5);
+argumentPerigee   = COE(6);
+mean_anomaly      = COE(7);
+
+%% ---Newton's Method--- %%
+
+% Initialize
+E = 0;
+previous_E = E;
+current_error = inf;
+acceptable_error = 10^6;
+
+% Numerical Solver
+while current_error > acceptable_error
+    f_approx_E       = E - eccentricity*sin(E) - mean_anomaly;
+    f_deriv_approx_E = 1 - eccentricity*cos(E);
+
+    E = previous_E - f_approx_E / f_deriv_approx_E;
+
+    current_error = abs(E - previous_E);
+    previous_E = E;
+end
+eccentric_anomaly = E;
+
+%% ---Solve for Initial State Vector--- %%
+
+% DOUBLE CHECK THESE
+rP = semi_major_axis * (cos(E) - eccentricity);
+rQ = semi_major_axis * sqrt(1-eccentricity^2) * sin(eccentric_anomaly);
+rW = 0;
+rPQW = [rP;
+        rQ;
+        rW];
+
+r = semi_major_axis * (1 -eccentricity*cos(E));
+
+vP = -sqrt(muEarth*semi_major_axis) / r * sin(E);
+vQ = sqrt(muEarth*semi_major_axis*(1-eccentricity^2)) / r * cos(E);
+vW = 0;
+vPQW = [vP;
+        vQ;
+        vW];
+
+% Rotation Matrix from Perifocal to Earth Centered Inertial Frame?
+R11 = cos(RAAN)*cos(argumentPerigee) - sin(RAAN)*sin(argumentPerigee)*cos(inclination);
+R12 = -cos(RAAN)*sin(argumentPerigee) - sin(RAAN)*cos(argumentPerigee)*cos(inclination);
+R13 = sin(RAAN)*sin(inclination);
+R21 = sin(RAAN)*cos(argumentPerigee) + cos(RAAN)*sin(argumentPerigee)*cos(inclination);
+R22 = -sin(RAAN)*sin(argumentPerigee) + cos(RAAN)*cos(argumentPerigee)*cos(inclination);
+R23 = -cos(RAAN)*sin(inclination);
+R31 = sin(argumentPerigee)*sin(inclination);
+R32 = cos(argumentPerigee)*sin(inclination);
+R33 = cos(inclination);
+
+R_PQW2IJK = [R11 R12 R13;
+             R21 R22 R23;
+             R31 R32 R33];
+
+% Rotate to ECI Frame
+r_eci = R_PQW2IJK*rPQW;
+v_eci = R_PQW2IJK*vPQW;
+
+r0 = r_eci;
+v0 = v_eci;
+
+end
+
+
+
+
+
---
+++ SimInitializer.m
@@ -0,0 +1,30 @@
+clc
+close all
+
+%% ---Initialize Constants--- %%
+muEarth = 3.986004418e5; % Standard gravitational parameter (Earth) [km^3/s^2]
+
+%% ---Initialize Unit Conversions--- %%
+d2r = pi / 180;           % Degrees to radians
+rev2r = 2*pi / (24*60^2); % Revolutions per day to radians per second
+
+%% ---Extract Classical Orbital Elements (COEs) from Two Line Element (TLE) %%
+tle = readmatrix("TLE_SharkSat.txt");
+
+mean_motion = tle(2,8) * rev2r;                    % [radians/sec]
+semi_major_axis = (muEarth / mean_motion^2)^(1/3); % [m]
+eccentricity = tle(2,5);                           % [dimensionless]
+inclination = tle(2,3)*d2r;                        % [radians]
+RAAND = tle(2,4)*d2r;                              % [radians]
+argument_of_periapsis = tle(2,6)*d2r;              % [radians]
+mean_anomaly = tle(2,7) * d2r;                     % [radians]
+
+% Assemble COE list Size: [7x1]
+COE = [mean_motion, semi_major_axis, eccentricity, inclination, RAAND, argument_of_periapsis, mean_anomaly];
+
+%% ---Obtain initial state vector [position; velocity] given COEs---%%
+[r0, v0] = COE2RV(muEarth, COE);
+orbitState0 = [r0;
+               v0];
+
+% output_data = sim('ADCSWorkshop2026_6DOFSim.slx');
